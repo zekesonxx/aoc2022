1af49168071091badbc2a744ebd71582f32ec064
Zoey Mertes <zmertes@zmertes.net>
2022-11-30 20:55:23 -0800

    update some rustisms

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-extern crate aoc_runner;
-
 #[macro_use]
 extern crate aoc_runner_derive;
 #[allow(unused_imports)]
 #[macro_use]
 extern crate lazy_static;
-extern crate nom;
-extern crate rayon;
-extern crate petgraph;
-extern crate itertools;
 
 pub mod day1;
 
