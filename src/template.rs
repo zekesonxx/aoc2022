4ade569e8867badda85f0b812b326403dea26415
Zoey Mertes <zmertes@zmertes.net>
2022-12-11 02:51:59 -0800

    Add newday script

--- src/template.rs
+++ src/template.rs
@@ -1,15 +1,15 @@
 
-#[aoc_generator(day1)]
+#[aoc_generator(dayDAYNUMBER)]
 pub fn gen(input: &str) -> Vec<usize> {
         input.split('\n').map(|x| x.parse().unwrap_or(0)).collect()
 }
 
-#[aoc(day1, part1)]
+#[aoc(dayDAYNUMBER, part1)]
 pub fn part1(input: &[usize]) -> usize {
 	0
 }
 
-#[aoc(day1, part2)]
+#[aoc(dayDAYNUMBER, part2)]
 pub fn part2(input: &[usize]) -> usize {
 	0
 }
@@ -22,9 +22,9 @@ mod tests {
 	4
 	5"};
 
-	aoc_tests!(day 1 sample1, EXAMPLE=0; gen:part1);
-	//aoc_tests!(day 1 part1, puzzle=0; gen:part1);
+	aoc_tests!(day DAYNUMBER sample1, EXAMPLE=0; gen:part1);
+	//aoc_tests!(day DAYNUMBER part1, puzzle=0; gen:part1);
 
-	aoc_tests!(day 1 sample2, EXAMPLE=0; gen:part2);
-	//aoc_tests!(day 1 part2, puzzle=0; gen:part2);
+	aoc_tests!(day DAYNUMBER sample2, EXAMPLE=0; gen:part2);
+	//aoc_tests!(day DAYNUMBER part2, puzzle=0; gen:part2);
 }
